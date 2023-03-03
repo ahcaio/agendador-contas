637db2056127b004f6d2853a281648167e98fcd0
Caio Cesar <ahcaio@hotmail.com>
2023-03-03 08:36:37 -0300

    first commit

---
+++ src/main/java/br/com/caio/agendadorcontas/AgendadorContasApplication.java
@@ -0,0 +1,13 @@
+package br.com.caio.agendadorcontas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorContasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/caio/agendadorcontas/AgendadorContasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caio.agendadorcontas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
